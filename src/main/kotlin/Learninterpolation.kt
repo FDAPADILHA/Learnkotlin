d01ee291a11ec0ded36c736fae54137507accdde
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-05-18 17:53:34 -0300

    Learning Interpolation

---
+++ src/main/kotlin/Learninterpolation.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    //CONCATENANDO STRINGS
+    val nome: String
+    val sobrenome: String
+
+    nome = "Filipe"
+    sobrenome = "Padilha"
+
+    println("Eu sou " + nome + " " + sobrenome)
+
+    //INTERPOLAÇÃO
+    println("Eu sou $nome $sobrenome")
+
+}
---
+++ src/main/kotlin/Learnmutablevar.kt
@@ -0,0 +1,17 @@
+ fun main(){
+
+     var nome: String = "Filipe"
+     println(nome)
+     nome = "Padilha"
+     println(nome)
+
+     var apelido = "Padilha"
+     apelido = "Lipe"
+     println(apelido)
+
+     val teste = "CASA"
+     println(teste)
+     //Change to var por causa que a varíavel é inalterável
+     //teste = "PORTA"
+
+ }
