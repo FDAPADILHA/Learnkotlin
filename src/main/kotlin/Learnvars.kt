56cc8cdf7f09f302cf810d23e3793d793b6e9fe5
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-05-18 14:10:23 -0300

    Learning details about variables

--- src/main/kotlin/Learnvars.kt
+++ src/main/kotlin/Learnvars.kt
@@ -11,4 +11,13 @@ fun main() {
     var long = 79L
     var long2: Long = 19999999999999
     var byte: Byte = 20
+
+    // Variation of data types
+
+    println("Int MAX - ${Int.MAX_VALUE} - ${Int.MIN_VALUE}")
+    println("ULong MAX - ${ULong.MAX_VALUE} - ${ULong.MIN_VALUE}")
+    println("UInt MAX - ${UInt.MAX_VALUE} - ${UInt.MIN_VALUE}")
+    println("UShort MAX - ${UShort.MAX_VALUE} - ${UShort.MIN_VALUE}")
+    println("UByte MAX - ${UByte.MAX_VALUE} - ${UByte.MIN_VALUE}")
+
 }
