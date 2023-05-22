ee518bdd854cd7b6e47108141e8b4181b5602a14
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-05-22 20:47:32 -0300

    Learning basic arithmetic operators

---
+++ src/main/kotlin/Learnarithmetichoperators.kt
@@ -0,0 +1,40 @@
+fun main(){
+
+    //Não soma
+    var mousetest = 325
+    mousetest + 1
+
+    println(mousetest)
+
+    //A variável devce receber ela somada a um valor
+    var mouse = 325
+
+    mouse = mouse + 3
+    println(mouse)
+    mouse = mouse - 3
+    println(mouse)
+    mouse = mouse * 5
+    println(mouse)
+    mouse = mouse / 5
+    println(mouse)
+
+    //Simplificando
+    mouse += 3
+    println(mouse)
+    mouse -= 3
+    println(mouse)
+    mouse *= 5
+    println(mouse)
+    mouse /= 5
+    println(mouse)
+
+    //Operação Resto %
+    var mousefloat: Float = 10f
+    mousefloat /= 3
+
+    println(mousefloat)
+
+    mouse = 50 % 15
+    println(mouse)
+
+}
