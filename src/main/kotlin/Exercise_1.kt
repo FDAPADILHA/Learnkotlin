f251c2492220176e1df01281e0bfd2d863d45165
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-05-22 21:53:18 -0300

    Exercise 1

---
+++ src/main/kotlin/Exercise_1.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    //EXERCISE 1
+
+    //a) Crie um novo arquivo com uma função main.
+        //arquivo Exercise_1 criado
+
+    //b) Declare uma variável mutável capaz de armazenar seu nome completo.
+        var nomeCompleto : String = "Filipe do Amaral Padilha"
+        println(nomeCompleto)
+
+    //c) Declare uma variável de texto sem valor algum.
+        var string : String
+
+    //d) Declare uma variável imnutável com o menor tipo de dado possível
+       //capaz de armazenar o número que você calça.
+
+    //e) Declare uma variável capaz de armazenar o PIB do Brasil(1.869.000.000.000).
+
+    //f) Declare uma variável capaz de armazenar a população do Brasil(211.000.000).
+
+    //g)Imprima o valor do PIB per capita.
+
+    //h)Rode seu programa de maneira que não tenha erros de compilação ou execução.
+}
