636777d9e1630fd9a0d68dd70a57115c2044669a
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-08-25 01:23:04 -0300

    Learning functions

--- src/main/kotlin/Exercise_1.kt
+++ src/main/kotlin/Exercise_1.kt
@@ -1,3 +1,5 @@
+import java.lang.management.ThreadInfo
+
 fun main(){
 
     //EXERCISE 1
@@ -10,16 +12,29 @@ fun main(){
         println(nomeCompleto)
 
     //c) Declare uma variável de texto sem valor algum.
-        var string : String
+        var variavel : String
 
     //d) Declare uma variável imnutável com o menor tipo de dado possível
        //capaz de armazenar o número que você calça.
+        val tam : Byte = 40
 
     //e) Declare uma variável capaz de armazenar o PIB do Brasil(1.869.000.000.000).
+        var pibbr : String
+        pibbr = "1.869.000.000.000"
+        println(pibbr)
 
     //f) Declare uma variável capaz de armazenar a população do Brasil(211.000.000).
+        var popbr : String = "211.000.000"
+        println(popbr)
 
     //g)Imprima o valor do PIB per capita.
+        val pib : Long = 1869000000000
+        val pop : Int = 211000000
 
+        var resposta : String = (pib / pop).toString()
+
+        println("PIB per capita: " + "R$" + resposta)
     //h)Rode seu programa de maneira que não tenha erros de compilação ou execução.
+
+
 }
---
+++ src/main/kotlin/LearnFunctions.kt
@@ -0,0 +1,14 @@
+fun main(){
+    helloWolrd()
+    sum(10,5)
+    print("A soma de 10 e 5")
+}
+
+fun sum(a : Int, b : Int) : Int{
+    val c = (a + b)
+    return (c)
+}
+fun helloWolrd() {
+    println("Hello World")
+
+}
