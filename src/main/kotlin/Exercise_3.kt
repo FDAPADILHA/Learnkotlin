810ab5e29cbefab6bafccaa328910849c0f4d1c2
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2023-09-08 17:05:25 -0300

    Exercise_3 if and else

---
+++ src/main/kotlin/Exercise_3.kt
@@ -0,0 +1,29 @@
+//Exercise 3
+// Escreva um programa para ajudar a empresa XPTO calcular o bônus que os funcionários receberão no final do ano.
+// Os bônus são classificados por cargo.
+//a. Gerentes recebem R$2000,00
+//b. Coordenadores recebem R$1500,00
+//c. Engenheiros de software recebem R$1000,00
+//d. Estagiários recebem R$500,00
+
+//Modifique o primeiro exercício para considerar o tempo de experiência no cálculo de bônus.
+//a. Gerentes com menos de 2 anos de experiência recebem R$ 2000,00. Caso Contrário recebem R$ 3000,00
+//b. Coordenadores com menos de 1 ano de experiência recebem R$1500,00, caso contrário recebem R$1800,00
+//c. Engenheiros de software recebem R$1000,00
+//d. Estagiários recebem R$500,00
+
+////Qual o resultado das comparações abaixo?
+////a.(a && b && c && d)
+////b.(!a && !b (c && d))
+////c.(a && ((b || c) || d))
+////d.(a || ((!b && c) || !d))
+
+
+fun bonusFinalAno(){
+
+}
+
+fun main(){
+
+
+}
